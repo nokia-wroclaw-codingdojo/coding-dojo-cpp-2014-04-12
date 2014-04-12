3cf5fc05fa58cb705eaed4bfd9106b0ce407a712
unknown <bedla@CNU24691KQ.nsn-intra.net>
2014-04-12 11:13:26 +0200

    ML:constructor_test

--- SoundexDojo/test/testCase/SoundexTest.cpp
+++ SoundexDojo/test/testCase/SoundexTest.cpp
@@ -1,5 +1,5 @@
 #include <boost/test/unit_test.hpp>
-
+#include<class/Soundex.hpp>
 /*                             The Three Laws of TDD
 * 1. You are not allowed to write any production code unless it is to make a failing unit test pass.
 * 2. You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
@@ -12,7 +12,11 @@ namespace
 
     BOOST_AUTO_TEST_CASE( empty )
     {
-        BOOST_CHECK(false);
+        Soundex* ptr = NULL;
+        BOOST_CHECK_MESSAGE(ptr == NULL, "ptr is not NULL!");
+        ptr = new Soundex();
+        BOOST_CHECK_MESSAGE(ptr != NULL, "ptr is NULL!");
+        delete ptr;
     }
 
     BOOST_AUTO_TEST_SUITE_END();
